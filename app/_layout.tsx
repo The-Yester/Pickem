cc8d8035031029b409fcbb5eeccfcb64055e9575
Ryan Yester <lkyester@gmail.com>
2025-07-10 17:36:08 -0500

    chore: Clean and reinstall all dependencies

--- app/_layout.tsx
+++
@@ -1,7 +0,0 @@
-// In app/_layout.js (or app/_layout.tsx)
-
-import RootAppNavigator from '../App'; // Assuming your App.js is in the parent (root) directory
-
-export default function RootLayout() {
-  return <RootAppNavigator />;
-}
